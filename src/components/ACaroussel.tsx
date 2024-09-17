d736834f13f55295d64d01b63fbfedc5a362ec8f
Antoine Chagnon-Michaud <antchmcg@cegeprdl.ca>
2024-09-17 10:13:49 -0400

    readded the html to the caroussel

--- src/components/ACaroussel.tsx
+++ src/components/ACaroussel.tsx
@@ -28,14 +28,43 @@ const ACaroussel = () => {
     backgroundColor: `grey`,
   }
   return (
-    <div className="caroussel flex-between">
+    <div className="card flex-between" style={style}>
       <div className="content">
-        <h2>Trouve ta classe!</h2>
-        <p>Ton code ici</p>
+        <h2>Mage </h2>
+        <p>Description: Un maître des arcanes capable de lancer de puissants sorts</p>
+        <div className="details">
+          <div className="stats">
+            <h2>Statistiques</h2>
+            <p>Health: 60</p>
+            <p>Mana: 120</p>
+            <p>Strength: 5</p>
+            <p>Intelligence: 18</p>
+            <p>Dexterity: 8</p>
+            <p>Wisdom: 15</p>
+          </div>
+          <div className="ability">
+            <h2>Talents</h2>
+            <ul>
+              <li>Boule de feu</li>
+              <li>Bouclier magique</li>
+              <li>Téléportation</li>
+              <li>Projectile magique</li>
+            </ul>
+          </div>
+          <div className="equipment">
+            <h2>Équipement</h2>
+            <ul>
+              <li>weapon : Bâton magique</li>
+              <li>armor : Robe d'apprenti</li>
+              <li>accessory : Anneau de concentration</li>
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="flex-between">
-        <button>&lt;---</button>
-        <button>---&gt;</button>
+        <button>Précédent</button>
+        <button>Cacher les détails{/* Afficher les détails */}</button>
+        <button>Suivant</button>
       </div>
     </div>
   )
