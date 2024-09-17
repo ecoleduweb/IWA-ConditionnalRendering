a44656119c3a3b6f4d4e660d7460e8ecce859190
Antoine Chagnon-Michaud <antchmcg@cegeprdl.ca>
2024-09-17 09:54:44 -0400

    removed ACard

--- src/components/ACaroussel.tsx
+++ src/components/ACaroussel.tsx
@@ -5,7 +5,22 @@ import { CharacterClass } from '../types/character';
 import { useState } from 'react';
 const characters: CharacterClass[] = charactersJson;
 const characterList = ['mage', 'guerrier', 'cleric', 'voleur']
-// 1. Fait fonctionner les boutons previous et next pour afficher chacun des personnage qu'on peut utiliser.
+// 1. Prend le temps de bien lire le code. S'il y a une ligne que tu ne comprends pas, CHAT GPT!
+// 2. À patrir de l'objet character, va chercher l'objet mage.
+//    Va lire les deux premières lignes de code sur ce site : https://sentry.io/answers/find-object-by-property-in-javascript-array/
+// 3. Crée une variable style qui contient un objet avec une propriété backgroundImage qui contient l'url de l'image du mage (/mage.jpg).
+//    La variable style est déjà affectée au tout premier div du html. On peut donc ajouter des éléments de style à partir du code au besoin!
+// QUESTION : Quand dois-tu prendre une classe conditionnelle ou un style conditionnel (exemple de classe conditionnel? L'exemple avec le dark mode)?
+// 4. À partir de l'objet, modifie le html pour afficher les différentes informations du mage.
+// 5. Fait fonctionner le bouton cacher les détails pour afficher ou cacher les détails du mage.
+//    Voici un lien pour t'aider à comprendre le conditionnal rendering https://react.dev/learn/conditional-rendering
+//    N'oublie pas de changer le texte du bouton!
+///////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////Deuxième partie//////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+// 1. Fait fonctionner les boutons previous et next pour afficher les informations des autres personnages.
+// 2. ohoh! Il semble que les objets n'ont pas tous le même nombre d'abilités? Comment pourrais-tu faire pour afficher les abilités de manière dynamique?
+// 3. BONUS on pourrait faire la même chose avec l'objet équipement?
 
 const ACard = () => {
   const style = {
